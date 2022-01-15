67ab099ec4fc9bd4b1b528473ec93fc66988434e
jaecheol <wocjf0513@naver.com>
2022-01-15 23:37:05 +0900

    baekjoon sol

--- baekjoon_1541.java
+++ baekjoon_1541.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
+public class baekjoon_1541 {
     public static void main(String[] args) throws IOException{
         BufferedReader bf=new BufferedReader(new InputStreamReader(System.in));
         String s=bf.readLine();
